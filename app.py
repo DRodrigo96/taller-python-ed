6042ef127f60b9d18ecf674e70ec366acf59aa5a
Rodrigo Sanchez <rodrigosanchezn@hotmail.com>
2022-02-26 02:56:01 -0500

    new model

--- app.py
+++ app.py
@@ -63,12 +63,12 @@ def home():
 # Execute application
 if __name__ == '__main__':
     
-    # > localhost
+    # > localhost / Heroku
     # app.run(debug=True)
     
     # > Docker
     # app.run(debug=False, host='0.0.0.0')
     
-    # Heroku
+    # Heroku / Heroku containers
     port = os.environ.get('PORT', 5000)
     app.run(debug=False, host='0.0.0.0', port=port)
